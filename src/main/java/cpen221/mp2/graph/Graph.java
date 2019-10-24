5ae81ee058f7f7a0d229b4dbabe539588f5f9a57
Joshua Yellowley <Joshuayellowley@gmail.com>
2019-10-24 16:03:57 -0700

    Intermediate Deadline Push

--- src/main/java/cpen221/mp2/graph/Graph.java
+++ src/main/java/cpen221/mp2/graph/Graph.java
@@ -11,6 +11,184 @@ public class Graph<V extends Vertex, E extends Edge<V>> implements ImGraph<V, E>
 
     // TODO: Implement this type
 
+    ArrayList<V> vertices = new ArrayList<>();
+    ArrayList<E> edges = new ArrayList<>();
+
+    public Graph(){
+
+    }
+
+    /*
+     * Add a vertex to the graph
+     *
+     * @param v vertex to add
+     * @return true if the vertex was added successfully and false otherwise
+     */
+    public boolean addVertex(V v){
+        if(!vertices.contains(v)){
+            vertices.add(v);
+            return true;
+        } else {
+            System.out.println("Vertex already contained in the vertex list");
+            return false;
+        }
+    }
+
+    /**
+     * Check if a vertex is part of the graph
+     *
+     * @param v vertex to check in the graph
+     * @return true of v is part of the graph and false otherwise
+     */
+    public boolean vertex(V v){
+        return vertices.contains(v);
+    }
+
+    /**
+     * Add an edge of the graph
+     *
+     * @param e the edge to add to the graph
+     * @return true if the edge was successfully added and false otherwise
+     */
+    public boolean addEdge(E e){
+        if(!edges.contains(e)){
+            addVertex(e.v1());
+            addVertex(e.v2());
+            edges.add(e);
+            return true;
+        } else {
+            System.out.println("Edge already contained in the edges list");
+            return false;
+        }
+    }
+
+    /**
+     * Check if an edge is part of the graph
+     *
+     * @param e the edge to check in the graph
+     * @return true if e is an edge in the graoh and false otherwise
+     */
+    public boolean edge(E e) {
+        return edges.contains(e);
+    }
+
+    /**
+     * Check if v1-v2 is an edge in the graph
+     *
+     * @param v1 the first vertex of the edge
+     * @param v2 the second vertex of the edge
+     * @return true of the v1-v2 edge is part of the graph and false otherwise
+     */
+
+    public boolean edge(V v1, V v2){
+        for (E edge : edges) {
+            if (edge.v1() == v1 && edge.v2() == v2 || edge.v2() == v1 && edge.v1() == v2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Determine the length on an edge in the graph
+     *
+     * @param v1 the first vertex of the edge
+     * @param v2 the second vertex of the edge
+     * @return the length of the v1-v2 edge if this edge is part of the graph
+     */
+    public int edgeLength(V v1, V v2){
+        for (E edge : edges) {
+            if (edge.v1() == v1 && edge.v2() == v2) {
+                return (edge.v1().id() - edge.v2().id());
+            }
+        }
+        return 0;
+
+    }
+
+    /**
+     * Obtain the sum of the lengths of all edges in the graph
+     *
+     * @return the sum of the lengths of all edges in the graph
+     */
+    public int edgeLengthSum(){
+        int sum = 0;
+        for(int i = 0; i < edges.size(); i++){
+            sum += edges.get(i).length();
+        }
+        return sum;
+    }
+
+    /**
+     * Remove an edge from the graph
+     *
+     * @param e the edge to remove
+     * @return true if e was successfully removed and false otherwise
+     */
+    public boolean remove(E e){
+        for(int i = 0; i < edges.size(); i++){
+            if(e == edges.get(i)){
+                edges.remove(i);
+                return true;
+            }
+        }
+        System.out.println("The edge is not contained in this graph");
+        return false;
+    }
+
+    /**
+     * Remove a vertex from the graph
+     *
+     * @param v the vertex to remove
+     * @return true if v was successfully removed and false otherwise
+     */
+    public boolean remove(V v){
+        if(vertices.contains(v)){
+            vertices.remove(v);
+            return true;
+        } else {
+            System.out.println("Vertex is not contained in the vertex list");
+            return false;
+        }
+    }
+
+
+    /**
+     * Obtain a set of all vertices in the graph.
+     * Access to this set **should not** permit graph mutations.
+     *
+     * @return a set of all vertices in the graph
+     */
+    public Set<V> allVertices();
+
+    /**
+     * Obtain a set of all vertices incident on v.
+     * Access to this set **should not** permit graph mutations.
+     *
+     * @param v the vertex of interest
+     * @return all edges incident on v
+     */
+    public Set<E> allEdges(V v);
+
+    /**
+     * Obtain a set of all edges in the graph.
+     * Access to this set **should not** permit graph mutations.
+     *
+     * @return all edges in the graph
+     */
+    public Set<E> allEdges();
+
+    /**
+     * Obtain all the neighbours of vertex v.
+     * Access to this map **should not** permit graph mutations.
+     *
+     * @param v is the vertex whose neighbourhood we want.
+     * @return a map containing each vertex w that neighbors v and the edge between v and w.
+     */
+    Map<V, E> getNeighbours(V v);
+
+
+
     //// add all new code above this line ////
 
     /**
@@ -77,4 +255,5 @@ public class Graph<V extends Vertex, E extends Edge<V>> implements ImGraph<V, E>
             remove(trim);
         }
     }
+
 }
